dcaad86213e99efeb2a301ba57b1abafbddcb81a
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-05-26 03:51:05 -0300

    Criação da classe Task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
-server.get('/teste', (req, res) => {
-    res.send('TUDO CERTO COM NOSSA API!')
-});
+const TaskRoutes = require('./routes/TaskRoutes');
+server.use('/task', TaskRoutes);
 
 server.listen(3000, () => {
-    console.log('API ONLINE')
+    console.log('API ONLINE');
 })
---
+++ src/routes/TaskRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const TaskController = require('../controller/TaskController');
+const TaskValidation = require('../middlewares/TaskValidation')
+const MacaddressValidation = require('../middlewares/MacaddressValidation')
+
+//CRUD
+router.post('/', TaskValidation, TaskController.create);
+router.put('/:id', TaskValidation, TaskController.update);
+router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
+router.put('/:id/:done', TaskController.done);
+//FILTER
+router.get('/filter/all', MacaddressValidation, TaskController.all);
+router.get('/filter/late', MacaddressValidation, TaskController.late);
+router.get('/filter/today', MacaddressValidation, TaskController.today);
+router.get('/filter/week', MacaddressValidation, TaskController.week);
+router.get('/filter/month', MacaddressValidation, TaskController.month);
+router.get('/filter/year', MacaddressValidation, TaskController.year);
+
+module.exports = router;
